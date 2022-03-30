371d2de13e3b067924482c6decb7419028ad9992
cory-weller <cory.a.weller@gmail.com>
2022-03-30 14:46:25 -0400

    fix blast dir

--- src/get-blast.sh
+++ src/get-blast.sh
@@ -2,4 +2,4 @@
 
 wget -O blast.tar.gz wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.13.0/ncbi-blast-2.13.0+-x64-linux.tar.gz
 tar -zxvf blast.tar.gz
-mv ncbi-blast-2.13.0+ blast
+mv ncbi-blast-2.13.0+/* blast/ && rmdir blast/ncbi-blast-2.13.0+ && rm blast.tar.gz
