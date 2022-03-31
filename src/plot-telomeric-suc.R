0147f743880a91468b76bd97cc3ee7d06cda2aff
cory-weller <cory.a.weller@gmail.com>
2022-03-31 14:42:12 -0400

    update plot

--- src/plot-telomeric-suc.R
+++ src/plot-telomeric-suc.R
@@ -9,8 +9,9 @@ blast <- foreach(file=filenames, .combine='rbind') %do% {
     fread(file)
 }
 
-blast[, c('V6', 'V7', 'V8', 'V9', 'V10', 'V11', 'V12') := NULL]
-setnames(blast, c('query','db', 'number', 'target', 'PID', 'evalue', 'bits'))
+
+blast[, c('V7', 'V8', 'V9', 'V10', 'V11', 'V12') := NULL]
+setnames(blast, c('query','db', 'number', 'target', 'PID', 'length', 'evalue', 'bits'))
 
 filenames <- list.files(path='data/', pattern='*summary.txt', full.names=TRUE)
 nreads <- foreach(file=filenames, .combine='rbind') %do% {
@@ -28,8 +29,9 @@ dat[, plate := tstrsplit(query, split="_")[2]]
 dat[, well := tstrsplit(query, split="_")[3]]
 dat[, well := as.numeric(well)]
 
-dat.ag <- dat[, list(.N), by=list(query, target, plate, well, nReads)]
+dat.ag <- dat[PID > 98 & length > 100, list(.N), by=list(query, target, plate, well, nReads)]
+#dat.ag <- dat[bits>200, list(.N), by=list(query, target, plate, well, nReads)]
 
-ggplot(dat.ag, aes(x=nReads, y=N, color=target)) + geom_point(shape=21, alpha=0.85) +
-labs(x="total reads genome-wide", y="reads aligned to target via BLAST")
-
+ggplot(dat.ag, aes(x=nReads, y=N, color=target)) + geom_point(shape=21, alpha=0.50) +
+labs(x="total reads genome-wide", y="reads aligned to target via BLAST", title="PID > 98, length > 100") +
+facet_wrap(~target, ncol=2, scales='free')
