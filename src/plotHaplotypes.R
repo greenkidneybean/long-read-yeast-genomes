e98257909ce7c9a2e39c0c2144c38aa85f04930a
cory-weller <cory.a.weller@gmail.com>
2022-04-16 21:26:24 -0400

    generalize variables

--- src/plotHaplotypes.R
+++ src/plotHaplotypes.R
@@ -6,24 +6,40 @@ library(foreach)
 dat.all <- fread('data/3003_G1.blast')
 
 # only use perfect blast for now
-dat.all <- dat.all[V5 == 100]
-
 dat.all[, sample := tstrsplit(V1, split="_")[3]]
 dat.all[, sample := as.numeric(sample)]
+dat.all[, c('V1','V2') := NULL]
+setnames(dat.all, 'V4', 'contig')
+setnames(dat.all, 'V3', 'readID')
+setnames(dat.all, 'V5', 'PID')
+setnames(dat.all, 'V6', 'matchLength')
+dat.all[, c('V7','V8','V9','V10') := NULL]
+setnames(dat.all, 'V11', 'contigStart')
+setnames(dat.all, 'V12', 'contigEnd')
+setnames(dat.all, 'V13', 'Evalue')
+setnames(dat.all, 'V14', 'bits')
+
+minMatchLength <- 150
+minPID <- 98.0
+windowSize <- 30000
 
 o <- foreach(i=1:96, .combine='rbind') %do% {
-    dat <- dat.all[sample == i]
-    dat.perf <- dat[dat[, .I[V5==max(V5)], by=V3]$V1]
-    uniques <- dat.perf[, .N, by=V3][N==1, V3]
-    dat.uniques <- dat.perf[V3 %in% uniques]
-    dat.uniques[, tenKbBin := cut(V11, breaks=seq(0,1.6e6, 10000))]
-    dat.uniques[, tenKbBin := as.numeric(tenKbBin)]
-    dat.wide <- dcast(dat.uniques[, .N, by=list(V4, tenKbBin)], tenKbBin~V4)
-    dat.wide[is.na(tig00000001), tig00000001 := 0]
-    dat.wide[is.na(tig00000087j113rc), tig00000087j113rc := 0]
-    dat.wide[, pctContig001 := tig00000001 / (tig00000001 + tig00000087j113rc)]
-    dat.out <- dat.wide[, c("tenKbBin","pctContig001")]
-    dat.out[, sample := i]
+    dat <- dat.all[sample == i][matchLength >= minMatchLength][PID >= minPID]
+    dat.perf <- dat[dat[, .I[bits==max(bits)], by=readID]$V1]
+    dat.perf <- dat.perf[readID %in% dat.perf[, .N, by=readID][N==1, readID]]
+    #uniques <- dat.perf[, .N, by=readID][N==1, readID]
+    #dat.perf <- dat.perf[readID %in% uniques]
+    dat.perf[, genomeWindow := cut(contigStart, breaks=seq(0,1.6e6, windowSize))]
+    dat.perf[, genomeWindow := as.numeric(genomeWindow)]
+    dat.wide <- dcast(dat.perf[, .N, by=list(contig, genomeWindow)], genomeWindow~contig, fill=0)
+    setnames(dat.wide, c('genomeWindow', 'contig_A', 'contig_B'))
+    dat.wide[is.na(contig_A), contig_A := 0]
+    dat.wide[is.na(contig_B), contig_B := 0]
+    dat.wide[, total := contig_A + contig_B]
+    dat.wide[, fractionA := contig_A / (contig_A + contig_B)]
+    dat.wide[, sample := i]
 }
 
-ggplot(o, aes(x=tenKbBin, y=sample, fill=pctContig001)) + geom_tile()
+g <- ggplot(o[total > 5], aes(x=genomeWindow, y=sample, fill=fractionA)) + geom_tile()
+
+ggsave(g, file="haplotypes-test.png", width=24, height=18, units='cm')
