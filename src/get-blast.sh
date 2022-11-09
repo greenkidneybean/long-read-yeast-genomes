e2fb0bd6af85dc5f06f5335e2a9d2036156d8c7d
Cory Weller <cory.a.weller@gmail.com>
2022-11-09 11:35:25 -0500

    remove unused scripts

--- src/get-blast.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-wget -O blast.tar.gz wget https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.13.0/ncbi-blast-2.13.0+-x64-linux.tar.gz
-tar -zxvf blast.tar.gz
-mv ncbi-blast-2.13.0+/* blast/ && rmdir blast/ncbi-blast-2.13.0+ && rm blast.tar.gz
--- src/snakesubmit.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-snakemake -k -j 20 --max-jobs-per-second 1 --max-status-checks-per-second 1 --use-singularity --latency-wait=180 --cluster="sbatch -c {threads} --mem={resources.mem_mb} --time={resources.runtime_min}"
